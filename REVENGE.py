b8a2cac4aa41855c0fd1a88f81d01ea0c5d70994
Alexisclv <45903953+Alexisclv@users.noreply.github.com>
2020-11-18 21:14:57 +0100

    Initialisation de l'agent Revenge

--- REVENGE.py
+++ REVENGE.py
@@ -1,19 +1,31 @@
 #!/usr/bin/env python
 from __future__ import print_function, division 
 
+from random import randint
+import logging, json
 # this is main script
-# simple version
+
 
 import aiwolfpy
 import aiwolfpy.contentbuilder as cb
 
-myname = 'Amandine'
+
+'''
+Alexis
+L'agent agit en fonction des actions qui sont portées contre lui. 
+'''
+
+
+myname = 'renvenge{:02d}'.format(randint(0,99))
 
 class SampleAgent(object):
     
     def __init__(self, agent_name):
         # myname
         self.myname = agent_name
+        logging.basicConfig(filename=self.myname+'.log',
+                            level=logging.DEBUG,
+                            format='')
         
         
     def getName(self):
@@ -23,9 +35,24 @@ class SampleAgent(object):
         self.base_info = base_info
         # game_setting
         self.game_setting = game_setting
-        # print(base_info)
-        # print(diff_data)
         
+        print(base_info)
+        #print(diff_data)
+        print('\n',game_setting)
+
+        #mémorise l'id de l'agent
+        self.myid = base_info['agentIdx']
+        logging.debug('# INIT : I\'am agent {}'.format(self.myid))
+        self.player_total = game_setting['playerNum']
+
+        #Initialise une liste avec un score de haine pour chaques joueur
+        #On réduit son propre score afin de ne pas voter pour soit-même
+        self.player_score = [0]*self.player_total
+        self.player_score[self.myid-1] = -10000
+
+        #On selectionne le joueur avec le plus de haine 
+        self.hate = self.player_score.index(max(self.player_score)) + 1
+
     def update(self, base_info, diff_data, request):
         self.base_info = base_info
         # print(base_info)
