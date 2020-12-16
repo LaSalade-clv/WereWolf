fa5b7660334f15defaa8ddb509daf955facace70
Alexisclv <45903953+Alexisclv@users.noreply.github.com>
2020-12-16 02:11:39 +0100

    Ajout ContreCourant+ avancement penseur

--- ContreCourant.py
+++ ContreCourant.py
@@ -11,11 +11,12 @@ import aiwolfpy.contentbuilder as cb
 
 
 '''
-Alexis 
+Antoine
+L'agent n'est jamais d'accord avec les autres et va toujours choisir la personne qui a le moins de vote pour elle.
 '''
 
 
-myname = 'Penseur{:02d}'.format(randint(0,99))
+myname = 'ContreCourant{:02d}'.format(randint(0,99))
 
 class SampleAgent(object):
     
@@ -30,6 +31,7 @@ class SampleAgent(object):
     def getName(self):
         return self.myname
     
+
     def initialize(self, base_info, diff_data, game_setting):
         self.base_info = base_info
         # game_setting
@@ -45,72 +47,66 @@ class SampleAgent(object):
         self.player_total = game_setting['playerNum']
 
 
-        self.player_suspect = [0]*self.player_total
-        self.player_suspect[self.myid-1] = -10000
+        self.player_score = [0]*self.player_total
+        self.player_score[self.myid-1] = -10000
 
-        self.player_suivi = [0]*self.player_total
-        self.player_suivi[self.myid-1] = -10000
-
-        self.historique_vote = [0]*self.player_total
-
-        #On selectionne le joueur avec le plus de haine 
-        self.suspect = self.player_suspect.index(max(self.player_suspect)) + 1
 
     def update(self, base_info, diff_data, request):
         self.base_info = base_info
         # print(base_info)
         # print(diff_data)
-        logging.debug('# UPDATE jour : {}'.format('x'))
-        #logging.debug(base_info)
+        logging.debug('# UPDATE')
+        logging.debug(base_info)
 
         #On mémorise les gens mort en baissant leurs score de haine
         if (request == 'DAILY_INITIALIZE'):
             for i in range(self.player_total):
                 if (base_info['statusMap'][str(i+1)] == 'DEAD'):
-                    self.player_suspect[i] -= 10000
-                    self.player_suivi[i] -= 10000
-
-        if (base_info['myRole'] == 'SEER'):
-            for ligne in diff_data.values:
-             # on vérifie chaque entrée de divination
-                if (ligne[1] == 'divine'):
-                    if ('WEREWOLF' in ligne[5]):
-                        self.player_suspect[ligne[4]-1] += 10000
-                    else:
-                        self.player_suspect[ligne[4]-1] -= 10000
-
+                    self.player_score[i] -= 10000
 
         #On regarde dans diff_data pour les paroles / votes
         logging.debug(diff_data)
 
         for row in diff_data.itertuples():
             type = getattr(row,'type')
+            text = getattr(row,'text')
 
             #Lors du vote
             if (type == 'vote'):
-                voter = getattr(row,'idx')
                 target = getattr(row,'agent')
-                self.historique_vote[voter-1] = target
 
+                self.player_score[target-1] -= 5
 
-#Fonctionne pas
-                if getattr(row,'day') >= 2:
-                    if self.historique_vote[voter-1] == target:
-                        self.player_suspect[target-1] += 10000
+            #Lorsqu'ils parlent
+            elif (type == 'talk' and 'Agent' in text): 
+                #Ils ont parlé de moi
+                #cible = getattr(row,'agent')
 
-        logging.debug('suspect:')
-        logging.debug(self.historique_vote)
+                for x,n in enumerate(text):
+                    if n.isdigit():
+                        cible = int(text[x:x+1])
+                        break
+                    else:
+                        continue
 
-        logging.debug(self.player_suspect)
+                if 'WEREWOLF' in text or 'VOTE' in text:
+                    #On se fait accusé de loupgarou
+                    #On pense voter pour moi
+                    self.player_score[cible-1] -= 2
 
+                else:
+                    #On a arreté de parlé de moi 
+                    self.player_score[cible-1] -= 1
 
+        self.hate = self.player_score.index(max(self.player_score)) + 1
+        logging.debug('Hate Score: '+', '.join(str(x) for x in self.player_score))  
 
-         
 
     def dayStart(self):
         logging.debug('# DAYSTART')
         return None
     
+
     def talk(self):
         logging.debug('# TALK')
 
@@ -119,34 +115,43 @@ class SampleAgent(object):
             'ESTIMATE Agent[{:02d}] WEREWOLF',
             'VOTE Agent [{:02d}]',
         ]
-        logging.debug(hatecycle[randint(0,2)].format(self.suspect))
-        return hatecycle[randint(0,2)].format(self.suspect)
+        logging.debug(hatecycle[randint(0,2)].format(self.hate))
+        return hatecycle[randint(0,2)].format(self.hate)
     
+
     def whisper(self):
         logging.debug('# WHISPER')
-        return 'ATTACK Agent[{:02d}]'.format(self.suspect)
+        return 'ATTACK Agent[{:02d}]'.format(self.hate)
         
+
     def vote(self):
-        logging.debug('# VOTE: ')
-        return self.suspect
+        logging.debug('# VOTE: '+str(self.hate))
+        return self.hate
     
+
     def attack(self):
-        logging.debug('# ATTACK: ')
-        return self.suspect
+        logging.debug('# ATTACK: '+str(self.hate))
+        return self.hate
     
+
     def divine(self):
-        logging.debug('# DIVINE: ')
-        return self.suspect
+        logging.debug('# DIVINE: '+str(self.hate))
+        return self.hate
     
+
     def guard(self):
-        logging.debug('# GUARD: ')
-        return self.suspect
+        logging.debug('# GUARD: '+str(self.hate))
+        return self.hate
     
+
     def finish(self):
         return None
     
 
+
 agent = SampleAgent(myname)
+    
+
 
 # run
 if __name__ == '__main__':
